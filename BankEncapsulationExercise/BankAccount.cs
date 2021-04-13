ef4a1f14ddbe992b8fb4beb3a9be89ede8bdd6b7
Kemuro <antonkemuro@gmail.com>
2021-04-13 13:52:30 -0500

    created BankEncapuslationExercise

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationExercise
+{
+    class BankAccount
+    {
+        private double balance = 0;
+        private string password = "12345";
+        public string name;
+        public bool Verification()
+        {
+            Console.Write("Enter your password: ");
+            string response = Console.ReadLine();
+
+            if (response == password)
+            {
+                Console.WriteLine($"Welcome {name}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password.");
+                return false;
+            }
+        }
+        public void Deposit()
+        {
+            var verify = Verification();
+            double value;
+            double confirm;
+            if(verify)
+            {
+                do { 
+                    Console.WriteLine("How much would you like to deposit?");
+                    value = Convert.ToDouble(Console.ReadLine());
+
+                
+                    Console.WriteLine($"You have selected to deposit ${value} into your account.");
+                    Console.WriteLine("Please re-enter the amount to confirm your deposit.");
+
+                    confirm = Convert.ToDouble(Console.ReadLine());
+                    if (value == confirm)
+                    {
+                        balance += value;
+                        Console.WriteLine($"${value} has successfully been added to your account.");
+                    }
+                } while (value != confirm);
+            }
+            
+        }
+        public void WIthdraw()
+        {
+            var verify = Verification();
+            double value;
+            double confirm;
+            if (verify)
+            {
+                do
+                {
+                    Console.WriteLine("How much would you like to withraw?");
+                    value = Convert.ToDouble(Console.ReadLine());
+                    confirm = 0;
+
+                    if (balance >= value)
+                    {
+                        Console.WriteLine($"You have selected to withdraw ${value} from your account.");
+                        Console.WriteLine("Please re-enter the amount to confirm your withdrawl.");
+
+                        confirm = Convert.ToDouble(Console.ReadLine());
+                        if (value == confirm)
+                        {
+                            balance -= value;
+                        }
+                    }
+                    else
+                        Console.WriteLine("Error: ammount requested exceeds your current balance.");
+                } while (value != confirm);
+
+            }
+        }
+        public double GetBalance()
+        {
+            Verification();
+            Console.WriteLine($"Your balance: ${balance}");
+            return balance;
+        }
+
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+            BankAccount acct1 = new BankAccount();
+            acct1.name = "Anu Heru";
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("What can we help you with?");
+                Console.WriteLine("- Deposit \n" +
+                    "- Withdraw \n" +
+                    "- Check Balance \n" +
+                    "- Exit");
+                response = Console.ReadLine();
+                
+                switch (response.ToLower())
+                {
+                    case "deposit":
+                        acct1.Deposit();
+                        break;
+                    case "withdraw":
+                    case "withdrawl":
+                        acct1.WIthdraw();
+                        break;
+                    case "balance":
+                    case "check balance":
+                        acct1.GetBalance();
+                        break;
+                    case "exit":
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid input.");
+                        break;
+                }
+
+            } while (response.ToLower() != "exit");
+        }
+        
+    }
+}
